e9a9199080afd5881afe2f75032a4850fa11e894
valeriot30 <valeriotriolo3010@gmail.com>
2026-01-30 22:35:29 +0100

    handling traffic via med

---
+++ automation/handle_traffic.py
@@ -0,0 +1,58 @@
+import subprocess
+import shlex
+
+def frr_cmd(container, cmds):
+    full_cmd = ["docker", "exec", container, "vtysh"]
+    for c in cmds:
+        full_cmd.extend(["-c", c])
+    subprocess.run(full_cmd, check=True)
+
+def update_node_med(container, as_number, interface, med):
+
+    route_map = f"MED-{interface}"
+
+    cmds = [
+        "conf t",
+        f"route-map {route_map} permit 10",
+        f"set metric {med}",
+        "exit",
+        f"router bgp {as_number}",
+        f"neighbor {interface} remote-as {as_number}", # <--- AGGIUNGI QUESTO
+        f"neighbor {interface} route-map {route_map} out",
+        "exit",
+        "end",
+        "write"
+    ]
+
+    full_cmd = ["docker", "exec", container, "vtysh"]
+    for c in cmds:
+        full_cmd.extend(["-c", c])
+
+    print("\n Executed command: ")
+    print(" ".join(shlex.quote(x) for x in full_cmd))
+
+    subprocess.run(full_cmd, check=True)
+
+    subprocess.run(
+        ["docker", "exec", container, "vtysh",
+         "-c", f"clear bgp ipv4 unicast {interface} soft out"],
+        check=True
+    )
+
+    print(f"[OK] {container}: neighbor {interface} → MED {med}")
+
+
+
+update_node_med(
+    "clab-project-pe1",
+    65001,
+    "10.1.11.1",
+    10
+)
+
+update_node_med(
+    "clab-project-pe2",
+    65001,
+    "10.1.11.2",
+    80
+)
